4b06c2ee835855f2c64a8b2678dd7d78c18dba48
Arsham1024 <Arsham512@gmail.com>
2021-09-26 13:29:39 -0700

    request and HTML parser import

--- Project1_Crawler/crawler/crawler.py
+++ Project1_Crawler/crawler/crawler.py
@@ -1,8 +1,33 @@
 import requests
+from html.parser import HTMLParser
 
 # Stating URL
 seed = "https://www.apple.com/"
-pages = 3000
 
-r = requests.get(seed)
-print(r.text)
+# HTML Parser
+parser = HTMLParser
+
+# Max number of pages crawiling
+# change this later to 3000
+pages = 10
+
+# crawler function
+def crawler(MAX_Pages):
+    # getting the starter HTML and turning it into plain text
+    start_page = requests.get(seed).text
+    # To increment with each pages successfully crawled
+    pages_visited = 0
+    # To store all the links to crawl
+    links_tocrawl = []
+
+    # Need a breadth first search type of algorithm for this crawler
+    # These conditions should limit the amount of pages visited to maximum we want
+    while pages_visited < MAX_Pages and len(links_tocrawl) < MAX_Pages:
+        data = parser.feed(start_page)
+        tag
+
+
+
+
+if __name__ == "__main__":
+    crawler(pages)
