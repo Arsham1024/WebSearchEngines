467003b958ca42e2dcf8c64383fbb33975fae5b3
kph3 <73854528+kph3@users.noreply.github.com>
2021-09-27 12:33:53 -0700

    Create processor.py
    Python file for processing blocks of texts for Content Analysis of a web crawl.
    
    3 different crawls in different languages (1 English, plus 2 others). 
    For each analysis: 
    1.Zipf’slaw
    1.Calculate word frequencies and ranks using all textual content, i.e. not HTML tags (feel free to use any tool/language)
    2.Plot word frequencies and word ranks (feel free to use any tool/language)
    3.Check if crawled content follows Zipf’s law (i.e. compare with typical Zipf’s distribution)
    2.Heap’s Law
    1.Plot vocabulary growth by collection growth, e.g. as examples in class

---
+++ Project1_Crawler/word_processor/processor.py
@@ -0,0 +1,6 @@
+# Accepts English, Spanish, and lastly Farsi(non-latin script) 
+
+# nltk package must be installed 
+
+# Python Natural Language Processing NLTK for tokenization, stopwords
+import nltk
