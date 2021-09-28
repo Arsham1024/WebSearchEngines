7a338bc3b9d67623940e57ab40b59a97b5d1c743
Arsham1024 <Arsham512@gmail.com>
2021-09-27 19:16:50 -0700

    crawler can now use the while loop

--- Project1_Crawler/crawler/crawler.py
+++ Project1_Crawler/crawler/crawler.py
@@ -32,12 +32,17 @@ def crawler(MAX_Pages):
         text_page = requests.get(links_tocrawl[pages_visited]).text
         extract_links(links_tocrawl, text_page)
 
+        # Need to put "Done" in the array links_tocrawl once a page's extraction is complete
+        
+
         # End of each iteration add one to counter
         pages_visited += 1
-
+    print(len(links_tocrawl))
 
 # This method will extract only URLs and save them to the links to crawl array.
 def extract_links(links_tocrawl, text_page):
+    # For handling exeptions
+    successful = True
     # This will take out all the links on a page and store the in the links_tocrawl array to be crawled.
     for link in BeautifulSoup(text_page, parse_only=SoupStrainer('a'), features="html.parser"):
         if link.has_attr('href') and not link['href'].startswith("/") and not link['href'].startswith("#"):
