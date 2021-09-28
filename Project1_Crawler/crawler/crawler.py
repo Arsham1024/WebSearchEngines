df2aa4aeed7a7435c1632c24441f4a0a9181abe0
Arsham1024 <Arsham512@gmail.com>
2021-09-27 19:15:31 -0700

    crawler can now uses a while loop

--- Project1_Crawler/crawler/crawler.py
+++ Project1_Crawler/crawler/crawler.py
@@ -11,26 +11,29 @@ parser = HTMLParser
 
 # Max number of pages crawiling
 # change this later to 3000
-pages = 10
+pages = 1000
 
 # crawler function
 def crawler(MAX_Pages):
     # getting the starter HTML and turning it into plain text
-    text_page = requests.get(seed).text
+    # text_page = requests.get(seed).text
     # To increment with each pages successfully crawled
     pages_visited = 0
     # To store all the links to crawl
-    links_tocrawl = []
+    # initially only has the seed at index 0
+    links_tocrawl = [seed]
 
     # This function takes an array to save all the links extracted and an html page in text format (not URL!)
-    extract_links(links_tocrawl, text_page)
+
 
     # Need a breadth first search type of algorithm for this crawler
     # These conditions should limit the amount of pages visited to maximum we want
     while pages_visited < MAX_Pages and len(links_tocrawl) < MAX_Pages:
-        break
+        text_page = requests.get(links_tocrawl[pages_visited]).text
+        extract_links(links_tocrawl, text_page)
+
         # End of each iteration add one to counter
-        # pages_visited += 1
+        pages_visited += 1
 
 
 # This method will extract only URLs and save them to the links to crawl array.
