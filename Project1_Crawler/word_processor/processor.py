5f4612d30426221b3383b2b87c4ad78d747e1202
kph3 <kevintpham@cpp.edu>
2021-10-05 12:17:08 -0700

    added further text cleaning, and generally cleaned up code. ALSO added an .ipynb jupyter notebook for graphical analysis

--- Project1_Crawler/word_processor/processor.py
+++ Project1_Crawler/word_processor/processor.py
@@ -2,21 +2,18 @@
 # Accepts English, Spanish, and lastly Farsi(non-latin script)
 
 import os
-
-# Graphing for Zipf's + Heap's Law
-import pandas as pd
-import matplotlib.pyplot as plt
+import string
 
 # Language Processing
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 import re
 
 # Used for Counting the words
 from collections import Counter
 import itertools
+
 
 # Load Stored Language Stopwords
 def load_stopwords(lang):
@@ -35,6 +32,7 @@ def load_stopwords(lang):
         stops = []
     return stops
 
+
 # Import ALL text from repository directory into a simple string
 # path example "../repository/English"
 def import_text_as_string(text_path):
@@ -42,14 +40,14 @@ def import_text_as_string(text_path):
     for filename in os.listdir(text_path):
         with open(os.path.join(text_path, filename), 'r') as file:
             file_text = file.readlines()
-            string = string + ' '.join(file_text) # Append file content to list
+            string = string + ' '.join(file_text)  # Append file content list
     return string
 
 
 # Seperate words into list
-def tokenize(text_list):
-    re.split('\W+', text_list)
-    tokens = re.split('\W+', text_list)
+def tokenize(text):
+    re.split('\W+', text)
+    tokens = re.split('\W+', text)
     return tokens
 
 
@@ -59,35 +57,44 @@ def remove_stopwords(text, stoplist):
     return text_no_stops
 
 
-def html_text_only(string):
-    soup = BeautifulSoup(string, 'html.parser')
+# Remove Punctuation
+def remove_punct(text):
+    text_no_punct = [word for word in text if word.isalpha()]
+    return text_no_punct
+
+
+# Remove any single character
+def remove_single_letters(text):
+    # There is probably a better way to do this??
+    single = ["a", "b", "c", "d", "e", "f", "g",
+              "h", "i", "j", "k", "l", "m", "n",
+              "o", "p", "q", "r", "s", "t", "u",
+              "v", "w", "x", "y", "z"]
+    text_no_single = [word for word in text if word not in single]
+    return text_no_single
+
+
+# Return only Text, NO HTML
+def html_text_only(text):
+    soup = BeautifulSoup(text, 'html.parser')
     text_only = soup.get_text()
     return text_only
 
+
 # Calls previous methods, accepts languages like "english", "spanish", "farsi"
 # expects path "../repository/English/"
+# PIPELINE to Clean text
 def process_text(language, path):
     stopwords = load_stopwords(language)  # Load Stopwords
     text = import_text_as_string(path)  # Import crawled text
-    soup = BeautifulSoup(text, 'html.parser')  # soup object
-    text = soup.get_text()  # get only text (REMOVE HTML...)
+    text = html_text_only(text)
     text = text.lower()
     text = tokenize(text)  # Create list of each word
     text = remove_stopwords(text, stopwords)  # remove stopwords
-
+    text = remove_punct(text)  # remove non alphabet values
+    text = remove_single_letters(text)
     # Produce a Dictionary {word, word count}
     text = list(itertools.chain(text))
     text = Counter(text)
 
     return text
-
-
-english = process_text("english", "../repository/English/")
-print(english)
-
-spanish = process_text("spanish", "../repository/Spanish/")
-print(spanish)
-
-farsi = process_text("farsi", "../repository/Farsi/")
-print(farsi)
-
---
+++ Project1_Crawler/word_processor/readme.md
@@ -0,0 +1,7 @@
+the processor.py file contains all the methods for opening the data that is in
+repository/{Language} folder.
+
+It also contains the pipeline of cleaning operations.
+
+the .ipynb file is for easy implementation of processor.py methods to view
+PANDAS ans Plots for our research section
