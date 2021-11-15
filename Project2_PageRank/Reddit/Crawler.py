e9ded60ffcc9aca3ca13f840d1d9bc3db83bd2fc
Arsham Mehrani <spectre@Arshams-MacBook-Pro.local>
2021-11-14 17:10:30 -0800

    skeleton code

--- Project2_PageRank/Reddit/Crawler.py
+++ Project2_PageRank/Reddit/Crawler.py
@@ -1,3 +1,17 @@
+import bs4
 import scrapy
+from scrapy.utils import response
 
-print("hello")
+
+class RedditSpider(scrapy.Spider):
+    name = "reddit"
+
+    start_urls = ["https://www.reddit.com/r/computerscience/"]
+
+    def parse(self, response):
+        # List of links on the page
+        links = response.css("div::class=_1poyrkZ7g36PawDueRza-J _11R7M_VOgKO1RJyRSRErT3").get()
+        print(links)
+        
+
+RedditSpider.parse()
