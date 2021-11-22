390887a7148a5f7f8bfa1bc3d9249e75de20173c
timhswede <timhswede@gmail.com>
2021-11-21 18:45:30 -0800

    Cleaned up code, dynamic count size for unique links, remove links if there are no outlinks

--- Project2_PageRank/CPP/MainCPP.py
+++ Project2_PageRank/CPP/MainCPP.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 
+#Clean list for Outlinks
 def clean_list(list_):
     list_ = list_.replace(',', '","')
     list_ = list_.replace('[', '["')
     list_ = list_.replace(']', '"]')
     return list_
 
+#Check if outlink has correlation with unique link
 def boolean_df(lists_links, unique_links):
     bool_dict = {}
     # Loop through all the tags
@@ -23,16 +25,20 @@ def main():
     pd.set_option('display.width', None)
 
     #Read in list.csv
-    df = pd.read_csv('Data\listTest.csv')
+    df = pd.read_csv('Data\listTest1.csv', ).drop_duplicates(subset='Link')
+    #Remove link if there are no outlinks
+    df = df.loc[df["Outlink"] != '[]']
+    #Grab number of rows
+    index = df.index
+    number_of_rows = len(index)
 
+    #Clean and separate outlinks column
     df["Outlink"] = df["Outlink"].apply(eval)
     df["Outlink"].dtype
-    #Clean outlinks column
     clean_list(df)
 
     #Get all unique links using value
     unique_links = df["Link"].value_counts()
-    #print(unique_links)
 
     #Return bool if link points to outlink
     df_bool = pd.DataFrame(boolean_df(df["Outlink"], unique_links.keys()))
@@ -40,47 +46,45 @@ def main():
 
     #corr() correlates links
     df_corr = df_bool.corr()
-    # print(fruits_corr)
+    #print(df_corr)
 
     #Change bool to int
     df_int = df_bool.astype(int)
 
-    #Numpy dot matrix
-    # n = 1
-    # df_freq_mat = np.dot(1 / n, df_int)
-    # df_freq = pd.DataFrame(df_freq_mat, columns=unique_links.keys(), index=unique_links.keys())
-    # print(df_freq)
+    #Matrix ------------------------------------------------------------------------------------------------------------
 
-    #Count of Links n
+    #Divides links based on number of outlinks
     vector = np.array(df["Count"])
-    df_freq_mat = np.dot(1 , df_int)
-    # Links 1/n
+    df_freq_mat = np.dot(1, df_int)
+    #Outlinks 1/n
     df_freq_mat = (df_freq_mat / vector[np.newaxis])
     df_freq = pd.DataFrame(df_freq_mat, columns=unique_links.keys(), index=unique_links.keys())
-    # print(df_freq)
+    #print(df_freq)
 
-    #Matrix multiplication
-    b = np.array([1/3, 1/3, 1/3]).reshape(3,1)
+    #Create vector filled with 1's, divide by vector with number of total unique links
+    base_vector = np.full((number_of_rows, 1), 1, dtype=int)
+    vector = np.full((number_of_rows, 1), number_of_rows, dtype=int)
+    vector = base_vector / vector
+
     #Mulitplies by vector of outlink count, reshapes into column Nx1
-    df_div = np.sum(np.dot(df_freq_mat, b), axis=1).reshape((3,1))
+    df_div1 = np.sum(np.dot(df_freq_mat, vector), axis=1).reshape((number_of_rows, 1))
     print("Iteration 1:")
-    #print(df_div1)
-    print(pd.DataFrame(df_div))
-    df_div2 = np.sum(np.dot(df_freq_mat, df_div), axis=1)
+    print(df_div1)
+    print(pd.DataFrame(df_div1))
+    df_div2 = np.sum(np.dot(df_freq_mat, df_div1), axis=1)
     print("Iteration 2:")
     print(pd.DataFrame(df_div2))
 
     #Sort matrix
-    df_final = pd.DataFrame(df_div2, unique_links.keys()).stack().sort_values().reset_index()
+    df_final = pd.DataFrame(df_div2, unique_links.keys()).stack().sort_values(ascending=False).reset_index()
     #Keeps only top 100 links
     df_final =  df_final.head(100)
-    #print(df_final)
+    print(df_final)
     #Writes dataframe to csv
     df_final.to_csv('Data\listRanked.csv', header=['Link', 'Delete', 'PageRank'])
     #Reopens csv to name index column and remove 'Delete' column
     df_csv = pd.read_csv('Data\listRanked.csv')
     df_csv.drop('Delete', axis=1, inplace=True)
     df_csv.to_csv('Data\listRanked.csv', index=False, header=['Rank', 'Link', 'PageRank'])
-
 
 main()
